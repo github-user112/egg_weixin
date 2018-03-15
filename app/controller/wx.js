0267f3e6c5ffdca3f99a5ccd826fe2850ffd6d79
guopengju <guopengju@tbkt.cn>
2018-03-15 16:10:36 +0800

    增加 上传文件

--- app/controller/wx.js
+++ app/controller/wx.js
@@ -17,6 +17,71 @@ class WxController extends Controller {
       }
     }
   }
+  async receive () {
+    const {app, ctx} = this
+    const {signature, timestamp, nonce, echostr} = ctx.request.query
+    let req_xml = ctx.request.body.xml
+    console.dir(req_xml)
+    console.dir(ctx.app.cache)
+    let res_xml = `<xml>
+          <ToUserName><![CDATA[${req_xml.FromUserName}]]></ToUserName>
+          <FromUserName><![CDATA[${req_xml.ToUserName}]]></FromUserName>
+          <CreateTime>${req_xml.CreateTime}</CreateTime>
+          `
+    const result = await ctx.service.wxReceive.insert(req_xml)
+    if (result.affectedRows === 1) { // 插入成功
+      switch (req_xml.MsgType[0]) {
+        case 'event':
+          switch (req_xml.Event[0]) {
+            case 'subscribe': // 用户关注
+              res_xml += `<Content><![CDATA[感谢关注]]></Content> <MsgType><![CDATA[text]]></MsgType></xml>`
+              break
+            case 'unsubscribe':
+              break
+            case 'CLICK':
+
+              res_xml += `<Content><![CDATA[2354]]></Content> <MsgType><![CDATA[text]]></MsgType></xml>`
+              break
+          }
+
+          break
+        case 'text':
+          res_xml += `<Content><![CDATA[${req_xml.Content}]]></Content><MsgId>${req_xml.MsgId}</MsgId> <MsgType><![CDATA[${req_xml.MsgType}]]></MsgType></xml>`
+          break
+        case 'image':
+          res_xml += `<Image><MediaId><![CDATA[${req_xml.MediaId}]]></MediaId></Image><MsgId>${req_xml.MsgId}</MsgId> <MsgType><![CDATA[${req_xml.MsgType}]]></MsgType></xml>`
+          break
+      }
+      ctx.body = res_xml
+    } else {
+      ctx.body = 'success'
+    }
+
+  }
+  async token () {
+    const {app, ctx} = this
+    // console.dir('pathhhhhhhhhhhhhh'+ctx.href)
+    if (ctx.host === '123.206.56.48') {
+      // console.dir(ctx.app.cache)
+      let url = ctx.query.url
+      let timestamp = parseInt(new Date().getTime() / 1000) + ''
+      let nonceStr = Math.random().toString(36).substr(2, 15)
+      const {ticket} = ctx.app.cache // = Object.assign({},{signature, timestamp, nonce, echostr},ctx.app.cache)
+      let ret = {
+        jsapi_ticket: ticket,
+        nonceStr: nonceStr,
+        timestamp: timestamp,
+        url: url
+      }
+      var ori_str = raw(ret);
+      let sha1Code = crypto.createHash("sha1");
+      let signature = sha1Code.update(ori_str,'utf-8').digest("hex");
+
+      ctx.body = {timestamp,nonceStr,signature}
+    }else{
+      ctx.body = 'Error'
+    }
+  }
 }
 
 module.exports = WxController
--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
   router.get('/wx', controller.wx.index);
-  router.post('/wx', controller.wxReceive.index);
-  router.get('/wx_token', controller.wxToken.index);
+  router.post('/wx', controller.wx.receive);
+  router.get('/wx_token', controller.wx.token);
   router.post('/upload', controller.upload.index);
 };
